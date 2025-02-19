0e219098c490bf9bcd14f3240eb69d369916c594
hyesoo kim <khwater1017@naver.com>
2025-02-19 16:22:10 +0900

    [암호 검사기] 유지보수 1. PasswordStrengthMeter 객체를 생성하는 중복 코드 수정-> 필드 2. 메서드를 이용하여 중복 제거

--- TDD/demo/src/test/java/com/tdd/chap02/PasswordStrengthMeterTest.java
+++ TDD/demo/src/test/java/com/tdd/chap02/PasswordStrengthMeterTest.java
@@ -3,26 +3,27 @@ package com.tdd.chap02;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+    private void assertStrength(String password, PasswordStrength expStr){
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
     @Test
     // 첫 번쨰 테스트: 모든 규칙을 충족하는 경우
     void meetsAllCriteria_Then_Strong(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, result);
+        assertStrength("ab12!@AB",PasswordStrength.STRONG);
+        assertStrength("abc1Add",PasswordStrength.STRONG);
     }
 
     @Test
     // 두 번째 테스트: 길이만 8글자 미만이고 나머지 조건은 충족하는 경우
     void meetsOtherCriteria_except_for_Length_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab12!@A",PasswordStrength.NORMAL);
     }
 
     @Test
     // 세 번째 테스트: 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
     void meetsOtherCriteria_except_for_number_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab!@ABqwer");
         assertEquals(PasswordStrength.NORMAL,result);
     }
