862b819bfe36ff5f978ef612845b8be57d5d1909
hyesoo kim <khwater1017@naver.com>
2025-02-25 20:40:14 +0900

    [07 대역] 리포지토리를 가짜 구현으로 사용

--- TDD/demo/src/test/java/com/tdd/chapter07/UserRegister.java
+++ TDD/demo/src/test/java/com/tdd/chapter07/UserRegister.java
@@ -2,15 +2,21 @@ package com.tdd.chapter07;
 
 public class UserRegister {
     private WeakPasswordChecker passwordChecker;
+    private UserRepository userRepository;
 
-    public UserRegister(WeakPasswordChecker passwordChecker) {
+    public UserRegister(WeakPasswordChecker passwordChecker, MemoryUserRepository userRegister) {
         this.passwordChecker = passwordChecker;
+        this.userRepository = userRegister;
     }
 
-    public void  register(String id, String pw, String email){
+    public void register(String id, String pw, String email){
         if(passwordChecker.checkPasswordWeak(pw)){
             throw new WeakPasswordException();
         }
-
+        User user= userRepository.findById(id);
+        if(user != null) {
+            throw new DupIdException();
+        }
+        userRepository.save(new User(id,pw,email));
     }
 }
--- TDD/demo/src/test/java/com/tdd/chapter07/UserRegisterTest.java
+++ TDD/demo/src/test/java/com/tdd/chapter07/UserRegisterTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserRegisterTest{
@@ -11,9 +12,12 @@ public class UserRegisterTest{
     private StubWeakPasswordChecker stubPasswordChecker =
             new StubWeakPasswordChecker();
 
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+
     @BeforeEach
     void setUp(){
-        userRegister = new UserRegister(stubPasswordChecker);
+        userRegister = new UserRegister(stubPasswordChecker,
+                fakeRepository);
     }
 
 
@@ -26,4 +30,26 @@ public class UserRegisterTest{
             userRegister.register("id","pw","email");
         });
     }
+
+    @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+    @Test
+    void dupIdExists(){
+        //이미 같은 ID 존재하는 상황 만들기
+        fakeRepository.save(new User("id","pw1","email@email.com"));
+
+        assertThrows(DupIdException.class,()->{
+            userRegister.register("id","pw2","email");
+        });
+    }
+
+    @DisplayName("같은 ID가 없으면 가입 성공함")
+    @Test
+    void noDupId_RegisterSuccess(){
+        userRegister.register("id","pw","email");
+
+        User savedUser = fakeRepository.findBuId("id");
+        assertEquals("id",savedUser.getId());
+        assertEquals("email",savedUser.getEmail());
+    }
+
 }
